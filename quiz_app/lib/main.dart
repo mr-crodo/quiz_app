93644c4ef4aa2c33e669b988531b3ca86b012d90
Nasib Mammadov <bduti09@gmail.com>
2022-01-07 02:30:33 +0400

    fixed

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,153 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/home_page.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-main() => runApp(MyApp());
+void main() {
+  debugPaintSizeEnabled = false;
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sample App',
+      title: 'Quiz App',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: PersonWidget(),
+        primarySwatch: Colors.indigo,
+        textTheme: TextTheme(
+          caption: TextStyle(
+            fontSize: 22.0, 
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+        ),
+        fontFamily: 'Georgia',
+        ),
+
+      home: HomePage(),
     );
   }
-}
-
-class FavoriteWidget extends StatefulWidget {
-  @override
-  State<FavoriteWidget> createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = false;
-  int _favoritCount = 87654;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(children: <Widget>[
-      Container(
-          child: IconButton(
-        icon:
-            (_isFavorited ? Icon(Icons.favorite) : Icon(Icons.favorite_border)),
-        onPressed: _toggleFavorite,
-        color: Colors.red[500],
-      ),
-      ),
-      SizedBox(
-        width: 40,
-        child: Container(
-          child: Text('$_favoritCount'),
-        ),
-      ),
-    ]);
-  }
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _isFavorited = false;
-        _favoritCount -= 1;
-      } else {
-        _isFavorited = true;
-        _favoritCount += 1;
-      }
-    });
-  }
-}
-
-class PersonWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Япония, Старшая Некома'),
-      ),
-      body: Container(child: _buildMainColumn()),
-    );
-  }
-
-  Widget _buildMainColumn() => ListView(children: [
-        _buildTopImage(),
-        Center(
-            child: Container(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: _buildRating(),
-              ),
-              Card(
-                elevation: 5,
-                margin: const EdgeInsets.all(5),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: _buildAction(),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: _buildDesc(),
-              )
-            ],
-          ),
-        ))
-      ]);
-
-  Widget _buildTopImage() => Container(
-      child: Card(
-          margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-          elevation: 5,
-          child: Image.asset(
-            'assets/img/pngegg.png',
-            fit: BoxFit.cover,
-          )));
-
-  Widget _buildRating() => ListTile(
-        title: Text('Кантаи Лова',
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 18.0,
-            )),
-        subtitle: Text('Япония, Старшая Некома'),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[FavoriteWidget()],
-        ),
-      );
-
-  Widget _buildAction() =>
-      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
-        _buildButton('Блокирубщий', Icons.star, Colors.black),
-        _buildButton('Рост 188см', Icons.account_tree_rounded, Colors.black),
-        _buildButton('Возраст 18', Icons.backup_sharp, Colors.black),
-      ]);
-
-  Widget _buildButton(String label, IconData icon, Color color) => Column(
-        children: <Widget>[
-          Icon(
-            icon,
-            color: Colors.black,
-          ),
-          Container(
-            child: Text(
-              label,
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                color: color,
-              ),
-            ),
-          ),
-        ],
-      );
-
-  Widget _buildDesc() => Text(
-      'В 6 уроке немного поговорим про виды State, а так же про'
-      'управления состоянием. Выполним небольшую практику по верстке',
-      softWrap: true,
-      style: TextStyle(
-        fontSize: 16.0,
-      ));
 }
--- quiz_app/lib/pages/home_page.dart
+++ quiz_app/lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/models/Question.dart';
+import 'package:quiz_app/widgets/answer.dart';
+import 'package:quiz_app/widgets/progress_bar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,16 +9,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final QuestionData data = QuestionData();
+  int _countResult = 0;
+  int _questionIndex = 0;
+
+  List<Icon> _icons = [];
+
+  void _clearState() => setState(() {
+        _questionIndex = 0;
+        _countResult = 0;
+        _icons = [];
+      });
+
+  void _onChangeAnswer(bool isCorrect) => setState(() {
+        if (isCorrect) {
+          _icons.add(Icon(Icons.brightness_1, color: Color(0xFFbd27ff)));
+          _countResult++;
+        } else {
+          _icons.add(Icon(Icons.brightness_1, color: Color(0xFF000000)));
+        }
+
+        _questionIndex += 1;
+      });
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Тестирование'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Здесь что то будет'),
+        appBar: AppBar(
+          title: Text('Тестирование'),
         ),
-      ),
-    );
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+              color: const Color(0xff2a375a),
+              image: DecorationImage(
+                image: AssetImage('assets/img/pngegg.png'),
+                fit: BoxFit.contain,
+              )),
+          child: Column(children: <Widget>[
+            ProgressBar(
+              icons: _icons,
+              count: _questionIndex,
+              total: data.questions.length,
+            ),
+
+
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                data.questions[_questionIndex].title,
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ),
+            ...data.questions[_questionIndex].answers
+                .map((value) => Answer(
+                      title: value['answer'],
+                      onChangeAnswer: _onChangeAnswer,
+                      isCorrect: value.containsKey('isCorrect') ? true : false,
+                    ))
+                .toList(),
+            
+          ]),
+        ));
   }
 }
